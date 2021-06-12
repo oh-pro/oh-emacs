744591a4582f2bd6c002850856f89e0a2fb09244
Semin Oh <osmfirst@Seminui-MacBookAir.local>
2021-06-13 02:03:08 +0900

    emacs

--- myinit.el
+++ myinit.el
@@ -10,36 +10,34 @@
 
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2021/bin/universal-darwin"))
 
-;; AucTeX
-(setq TeX-auto-save t)
-(setq TeX-PDF-mode t)
-;; (setq TeX-parse-self t)
-;; (setq-default TeX-master nil)
-;; (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-;; (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-;; (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-;; (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(use-package latex
+  :defer t
+  :ensure auctex
+  :mode ("\\.tex\\'" . LaTeX-mode)
+  :bind
+  (:map LaTeX-mode-map
+	("M-n" . outline-next-heading)
+	("M-p" . outline-previous-heading)
+	("C-c C-c" . TeX-command-run-all)
+	("C-c l" . TeX-error-overview))
 
-(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-(setq TeX-source-correlate-start-server t)
-(setq TeX-source-correlate-method 'synctex)
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-PDF-mode t)
+  (setq TeX-parse-self t)
+  (setq TeX-save-query nil)
+  ;; (setq-default TeX-master nil) ;; Make emacs aware of multi-file projects
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX t)
 
-;; (setq reftex-plug-into-AUCTeX t)
+  (add-hook 'LaTeX-mode-hook (lambda ()
+			       (push
+				'("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t :help "Run latexmk on file")
+				TeX-command-list)))
+  (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
 
+  (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+  (setq TeX-view-program-list
+	'(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
 
-;; Use Skim as viewer, enable source <-> PDF sync
-;; make latexmk available via C-c C-c
-;; Note: SyncTeX is setup via ~/.latexmkrc (see below)
-;; (add-hook 'LaTeX-mode-hook (lambda ()
-;; 			     (push
-;; 			      '("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
-;; 				:help "Run latexmk on file")
-;; 			      TeX-command-list)))
-;; (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
-
-;; use Skim as default pdf viewer
-;; Skim's displayline is used for forward search (from .tex to .pdf)
-;; option -b highlights the current line; option -g opens Skim in the background  
-(setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
-(setq TeX-view-program-list
-      '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+  )
