112e5a881955cc52faf5623077b3054926291d88
Semin Oh <osmfirst@Seminui-MacBookAir.local>
2021-06-13 02:24:13 +0900

    emacs

--- myinit.el
+++ myinit.el
@@ -22,22 +22,22 @@
 	("C-c l" . TeX-error-overview))
 
   :config
-  (setq TeX-auto-save t)
   (setq TeX-PDF-mode t)
-  (setq TeX-parse-self t)
-  (setq TeX-save-query nil)
-  ;; (setq-default TeX-master nil) ;; Make emacs aware of multi-file projects
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (setq reftex-plug-into-AUCTeX t)
 
-  (add-hook 'LaTeX-mode-hook (lambda ()
-			       (push
-				'("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t :help "Run latexmk on file")
-				TeX-command-list)))
+  (add-hook 'LaTeX-mode-hook
+	    (lambda ()
+	      (push
+	       '("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
+		 :help "Run latexmk on file")
+	       TeX-command-list)))
   (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
 
   (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
   (setq TeX-view-program-list
 	'(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
 
+  (custom-set-variables
+   '(TeX-source-correlate-method 'synctex)
+   '(TeX-source-correlate-mode t)
+   '(TeX-source-correlate-start-server t))
   )
